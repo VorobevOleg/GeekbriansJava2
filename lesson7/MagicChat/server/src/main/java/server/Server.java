3bcd8c69778f21e03632a066f9d9552a80cc59a3
VorobevOleg <kukuruza-09@ya.ru>
2022-01-19 09:03:11 +0300

    v0

---
+++ lesson7/MagicChat/server/src/main/java/server/Server.java
@@ -0,0 +1,71 @@
+package server;
+
+//import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private ServerSocket server;
+    private Socket socket;
+    private final int PORT = 8189;
+
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public Server() {
+        clients = new CopyOnWriteArrayList<>();
+        authService = new SimpleAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Server stop");
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+    public void broadcastMsg(ClientHandler sender, String msg) {
+        String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+//    public void privateMsg (ClientHandler sender, String recipient, String msg) {
+//        String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
+//
+//    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+}
